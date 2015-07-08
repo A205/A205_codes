d293b84a71ad0366505626bd47414499665e8f12
hxj1119536619 <1119536619@qq.com>
2015-07-08 20:03:36 +0800

    exercise1
    第一天的作业

---
+++ huxiaojuan/day 1/exercise/Work2.java
@@ -0,0 +1,20 @@
+package exercise;
+
+public class Work2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            boolean x=true;
+            System.out.println(x);
+            x=false;
+            System.out.println(x);
+           while(x){
+            	System.out.println(1);
+            	 }
+            		System.out.println(0);
+		   }
+
+}
---
+++ huxiaojuan/day 1/exercise/Work3.java
@@ -0,0 +1,16 @@
+package exercise;
+public class Work3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	  	// TODO Auto-generated method stub
+		String i,j;
+		i="Ó¢ÎÄ×Ö·û";
+		j="ÖÐÎÄ×Ö·û";
+		System.out.println(i+"¡¢"+j);
+
+	}
+
+}
