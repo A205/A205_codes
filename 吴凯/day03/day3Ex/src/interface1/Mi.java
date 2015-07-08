3cfd53b79ff8f0345e2dae4fcb41deb396f6ee87
abudexiong <qmwk1120@gmail.com>
2015-07-08 19:13:10 +0800

    day3

---
+++ 吴凯/day03/day3Ex/src/interface1/Mi.java
@@ -0,0 +1,8 @@
+package interface1;
+
+public class Mi implements Usb{
+
+	public void A(){
+		System.out.println("ÎÒÊÇÀ×¾ü Are U ok£¿£¿£¿");
+	}
+}
---
+++ 吴凯/day03/day3Ex/src/多态/Fruit1.java
@@ -0,0 +1,17 @@
+package ¶àÌ¬;
+
+public abstract class Fruit1 {
+
+}
+class Apple extends Fruit1{
+	public String toString(){
+		return "Æ»¹û";
+	}
+}
+class Mango extends Fruit1{
+	public String toString(){
+		return "Ã¢¹û";
+	}
+}
+
+
